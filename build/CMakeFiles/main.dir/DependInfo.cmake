1068c04e2263fc2435700e644ea36400a0e9b7db
foulonj <Jeremie@JFoulon-2.local>
2014-08-31 19:18:10 +0200

    Free repository

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Jeremie/Codes/igl/src/light.cpp" "/Users/Jeremie/Codes/igl/build/CMakeFiles/main.dir/src/light.cpp.o"
+  "/Users/Jeremie/Codes/igl/src/main.cpp" "/Users/Jeremie/Codes/igl/build/CMakeFiles/main.dir/src/main.cpp.o"
+  "/Users/Jeremie/Codes/igl/src/object.cpp" "/Users/Jeremie/Codes/igl/build/CMakeFiles/main.dir/src/object.cpp.o"
+  "/Users/Jeremie/Codes/igl/src/properties.cpp" "/Users/Jeremie/Codes/igl/build/CMakeFiles/main.dir/src/properties.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././src /usr/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ src/object.cpp
@@ -0,0 +1,20 @@
+#include "object.hpp"
+
+Object::Object():name("test")
+{}
+
+/*Object::~Object(){
+  properties.clear();
+}
+
+void Object::addProperties(Properties p){
+  properties.push_back(p);
+}
+
+void Object::display(){
+  cout << "Display the Object with name: " << name << endl;
+  
+  cout << "Size of properties: " << properties.size() << endl;
+  for (unsigned int i=0; i < properties.size(); i++)
+    properties[i].display();
+}*/
